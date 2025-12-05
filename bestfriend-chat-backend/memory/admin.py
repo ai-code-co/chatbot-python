87b7f8c44f5c31ef4f0026ad21ed378b6a6922e7
pycode <medhavisharma11par@gmail.com>
2025-12-05 16:38:12 +0530

    admin setup

--- bestfriend-chat-backend/memory/admin.py
+++ bestfriend-chat-backend/memory/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserMemory, ConversationMessage, ConversationSummary
+
+
+
+admin.site.register(UserMemory)
+admin.site.register(ConversationMessage)
+admin.site.register(ConversationSummary)    
+
+# Register your models here.    
